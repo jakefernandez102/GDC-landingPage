96c7f28930a436d4c2ffeb50da42aaa2277e083f
Jakesteve102 <jake.fernandezbrizuela@outlook.com>
2023-11-16 21:15:20 -0600

    Creating and adding project to repo

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,26 @@ function App() {
       <div className='bg-purple-cus text-center'>
         <h2 className=' text-white uppercase tracking-wider'>Transformando ideas en Código</h2>
       </div>
-      
+
       <main className='flex flex-col gap-10 justify-center items-center'>
         <div className='w-full flex justify-center py-5'>
           <img className='' src="img/logo.webp" alt="GDC Logo" />
         </div>
         
-        <section className='grid grid-cols-5 items-center w-4/6 m-auto '>
+        <section className='md:grid md:grid-cols-5 md:items-center md:w-4/6 m-auto '>
           <div className='col-span-3'>
             <img src="/img/1.jpg" alt="code image" />
           </div>
-          <div className='col-span-2 flex flex-col gap-3 justify-center items-center bg-purple-cus h-full'>
+          <div className='p-4 col-span-2 flex flex-col gap-3 justify-center items-center bg-purple-cus h-full'>
             <h3 className='text-center text-white text-xl uppercase tracking-wider font-boldAZX'>Bienvenido</h3>
-            <p className='w-2/3 text-center text-white font-extrabold text-3xl '>Nuestro lanzamiento es inminente y tomará lugar muy pronto.</p>
+            <p className='w-2/3 text-center text-white font-extrabold text-sm md:text-xl'>Nuestro lanzamiento es inminente y tomará lugar muy pronto.</p>
           </div>
         </section>
 
           <div className='w-3/4 flex flex-col justify-center items-center '>
             <h3 className='font-bold text-3xl mt-14 mb-10'>Subscribete</h3>
             <p>Inscríbase para ser el primero en enterarse de nuestros eventos de lanzamiento.</p>
-            <form className='w-full flex gap-3'>
+            <form className='flex flex-col md:w-full md:flex gap-3'>
               <div className='w-full flex flex-col'>
                 <label htmlFor="">Email</label>
                 <input type="text" className='border-b-2 border-b-slate-300' />
